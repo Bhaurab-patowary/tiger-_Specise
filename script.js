caa86b4c06fccfa60640d76f3fec25ce0cad5f11
MAHAMMAD ANAS <mahammadanasofficial@gmail.com>
2024-11-24 19:32:42 +0530

    ttt

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function toggleMenu() {
-    const menu = document.querySelector('.menu');
-    menu.classList.toggle('active');
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburgerButton = document.getElementById("icon");
+    const navMenu = document.getElementById("nav-menu");
+  
+    hamburgerButton.addEventListener("click", function () {
+      navMenu.classList.toggle("menu-sm");
+
+      if (navMenu.classList.contains("menu-sm")) {
+       
+        navMenu.classList.remove("menu");
+      }
+      else{
+        navMenu.classList.add("menu");
+      }
+      console.log('hello');
+      
+    });
+  });
